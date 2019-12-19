54bb04a550a3d736bc74d07c3882346c09c48e1c
TheOnlyMrCat <signupforforums5@gmail.com>
2019-12-19 16:12:15 +1100

    Fixed node type discrepancies
    I was an idiot and put the parameters the wrong way around in the new constructor.
    Always use enum class kids

--- src/parse.cpp
+++ src/parse.cpp
@@ -10,7 +10,7 @@ extern std::FILE *yyin;
 
 int parse() {
     yyin = std::fopen(filename.c_str(), "r");
-    parseResult = std::unique_ptr<Node>(new Node(NodeType::NONE, 0, {}));
+    parseResult = std::unique_ptr<Node>(new Node(0, NodeType::NONE, {}));
 
     int result = yyparse();
 
@@ -23,12 +23,12 @@ int parse() {
 
 std::vector<std::string> typesList;
 
-std::string mapNodeType(int type) {
+std::string mapNodeType(NodeType type) {
     if (typesList.size() == 0) {
         std::istringstream is(std::string(reinterpret_cast<char*>(nodetypenames_txt), nodetypenames_txt_len), '\n');
         typesList = std::vector<std::string>(std::istream_iterator<std::string>{is}, std::istream_iterator<std::string>());
     }
-    return typesList[type].substr(0, typesList[type].length() - 1);
+    return typesList[static_cast<size_t>(type)].substr(0, typesList[static_cast<size_t>(type)].length() - 1);
 }
 
 void recursivePrint(std::unique_ptr<Node>& node, std::ofstream& out, int depth) {
@@ -40,24 +40,24 @@ void recursivePrint(std::unique_ptr<Node>& node, std::ofstream& out, int depth) 
         out << '-';
     }
 
-    out << '[' << mapNodeType(node->type) << "] <line:" << node->location.first_line << '-' << node->location.last_line << ", col:" << node->location.first_column << '-' << node->location.last_column << '>';
+    out << '[' << static_cast<unsigned int>(node->type) << ':' << mapNodeType(node->type) << "] <line:" << node->location.first_line << '-' << node->location.last_line << ", col:" << node->location.first_column << '-' << node->location.last_column << '>';
     switch (node->type) {
-        case DECL:
-        case PARAM:
-        case LIBNAME:
-        case FILEPATH:
-        case VALSTR:
-        case QUALPART:
-        case EXPRBASIC:
-        case EXPRASSIG:
-            out << ' ' << strings[node->value.valC];
+        case NodeType::DECL:
+        case NodeType::PARAM:
+        case NodeType::LIBNAME:
+        case NodeType::FILEPATH:
+        case NodeType::VALSTR:
+        case NodeType::QUALPART:
+        case NodeType::EXPRBASIC:
+        case NodeType::EXPRASSIG:
+            out << " -- " << strings[node->value.valC];
             break;
-        case RUN:
-        case VALINT:
-            out << ' ' << node->value.valI;
+        case NodeType::RUN:
+        case NodeType::VALINT:
+            out << " -- " << node->value.valI;
             break;
-        case VALFLOAT:
-            out << ' ' << node->value.valF;
+        case NodeType::VALFLOAT:
+            out << " -- " << node->value.valF;
             break;
         default:
             break;
